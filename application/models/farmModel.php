e7c9694eb971f9f9307d12e2655ae0a52dd9f519
NipunaC95 <chandimal.nipuna.1995@gmail.com>
2019-09-12 16:16:14 +0530

    Fixed card hover unserline  css

--- application/models/farmModel.php
+++ application/models/farmModel.php
@@ -2,7 +2,9 @@
 
     class farmModel extends CI_Model{
         public function insert($data){
+            $this->load->helper('url');
             $this->db->insert('farms',$data);
+            redirect("/main/enter");
         }
     }
 
