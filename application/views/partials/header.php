498a45fce29799d64e737b80ebf332e8cd970bab
NipunaC95 <chandimal.nipuna.1995@gmail.com>
2019-09-12 11:42:14 +0530

    git

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -3,7 +3,6 @@
   <head>   
         
         <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>">
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/view.css');?>">
         <script src="<?php echo base_url('assets/js/jq.js');?>"></script> 
   </head>
   <body>
