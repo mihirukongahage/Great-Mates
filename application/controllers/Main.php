c77cf3a885b33c32f317bc3a08c81dc60bea6b64
Mihiru <mihirukongahage@gmail.com>
2019-09-14 01:01:05 +0530

    test

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -5,7 +5,8 @@ class Main extends CI_Controller {
 
     public function __construct()
     {
-    parent::__construct();
+    
+        parent::__construct();
     
     $this->load->database();
     $this->load->helper('url');
