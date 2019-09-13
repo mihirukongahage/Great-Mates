4794fecb3c0f095f8565a284dbd61170598c7b28
NipunaC95 <chandimal.nipuna.1995@gmail.com>
2019-09-13 09:40:58 +0530

    Created records submitions

--- application/views/Records/piggery_records.php
+++ application/views/Records/piggery_records.php
@@ -10,17 +10,51 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.src.js"></script>
 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+
+    <style>
+    .card-body{
+      background-color: #DAAF8D;
+    }
+
+    body{
+      background-color: 	#C8DFD9;
+    }
+
+    .czard-body{
+      color: 	#0b2829;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+    
+     
+    .formContainer{
+        position:relative;
+        top:50px;
+        width:60%;
+        margin:auto;
+    }
+
+</style>
+
+
   </head>
   <body>
-    
-    <div class="container">
-        <h1>Monthly Records</h1>
-        <h5 class="card-subtitle mb-2 text-muted">Details related to piggery</h5>
-            <div class="mt-5">
-                                <form method="post" action="<?php echo base_url(); ?>index.php/main/signup_validate">
+    <div class="section offset-4 offset-sm-3 offset-md-3">
+    <br>
+      <h1>Monthly Records</h1>
+      <h4 class="card-subtitle mb-2 text-muted"></h4>
+
+        <div class="section">
+
+        <div class="formContainer">
+        
+            <form method="post" action="<?php echo base_url('index.php/monthlyRecordController/insertPiggery');?>">
                                     <div class="form-group">
                                         <label for="Farm_reg_no">Farm Registration Number</label>
-                                        <input type="text" name="farm_reg_no" class="form-control" id="farm_reg_no" placeholder="Farm Registration Number">
+                                        <input type="text" width="5" name="farm_reg_no" class="form-control" id="farm_reg_no" placeholder="Farm Registration Number">
                                         <!-- <span class="text-danger"><?php echo form_error('faarm_reg_no'); ?></span> -->
                                     </div>
                                     <div class="form-group">
@@ -34,23 +68,44 @@
                                         });
                                     </script>
                                     </div>
+ 
+                                    <div class="form-group"> 
+                                        <label for="sel1">Select month:</label>
+                                        <select class="form-control" id="sel1" name="month">
+                                          <option>January	    </option>
+                                          <option>February	</option>
+                                          <option>March	    </option>
+                                          <option>April	    </option>
+                                          <option>May	   	    </option>
+                                          <option>June	    </option>
+                                          <option>July	    </option>
+                                          <option>August	    </option>
+                                          <option>September	</option>
+                                          <option>October	    </option>
+                                          <option>November	</option>
+                                          <option>December	</option>
+                                        </select>
+                                      <!-- <span class="text-danger"><?php echo form_error('month'); ?></span> -->
+                                      </div>
+                                    
+
                                     <div class="form-group">
                                         <label for="No_of_animals">Number of Animals</label>
                                         <input type="number" name="no_of_animals" class="form-control" id="no_of_animals" placeholder="Number of Animals">
                                         <!-- <span class="text-danger"><?php echo form_error('no_of_animals'); ?></span> -->
                                     </div>
                                     <div class="form-group">
-                                        <label for="Meat_production">Meat Production</label>
+                                        <label for="Meat_production">Meat Production(Kilograms)</label>
                                         <input type="number" name="meat_production" class="form-control" id="meat_production" placeholder="Meat Production">
                                         <!-- <span class="text-danger"><?php echo form_error('meat_production'); ?></span> -->
                                     </div>
                                     <div class="form-group">
-                                        <label for="Expences">Expences</label>
+                                        <label for="Expences">Expences(LKR)</label>
                                         <input type="number" name="expences" class="form-control" id="expences" placeholder="Expences">
                                         <!-- <span class="text-danger"><?php echo form_error('expences'); ?></span> -->
                                     </div>
                                     <div class="form-group">
-                                        <label for="Meat_production">Income</label>
+                                        <label for="Meat_production">Income(LKR)</label>
                                         <input type="number" name="income" class="form-control" id="income" placeholder="Income">
                                         <!-- <span class="text-danger"><?php echo form_error('income'); ?></span> -->
                                     </div>
@@ -60,9 +115,14 @@
                                         echo $this->session->flashdata("error"); 
                                     ?>
                                     </div>
-                                </form>
-            </div>
-    </div>
+                                    <input type="hidden" name="typeOf" value="cattle">
+                                </form>   
+        </div>
+        </div>
+        <div class="mt-5"></div>
+
+   
+</div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
