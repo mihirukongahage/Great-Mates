69e141707fbddb17db0195b580ee1428dc6562ad
Mihiru <mihirukongahage@gmail.com>
2019-09-14 08:43:55 +0530

    changes

--- application/views/signup.php
+++ application/views/signup.php
@@ -1,4 +1,16 @@
-<?php include('partials/header.php')?>
+    <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://code.highcharts.com/highcharts.src.js"></script>
+    </head>
+  <body>
     <div class="row mt-5">
     <div class="col col-sm-12 col-md-6 col-lg-4 offset-md-2 offset-lg-3">
         <div class="card login-card" style="width: 50rem"">
@@ -68,4 +80,5 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
-<?php include('partials/footer.php')?>
+</body>
+</html>
