6ac06083a18d56617103eb3178b69c22cdf8605f
NipunaC95 <chandimal.nipuna.1995@gmail.com>
2019-10-23 17:07:04 +0530

    Min value = 0

--- application/views/AnnualReports/annual_report_goat.php
+++ application/views/AnnualReports/annual_report_goat.php
@@ -146,7 +146,55 @@
     <div class="section offset-4 offset-sm-3 offset-md-3">
       <h1>Annual report for goat</h1>
       <h4 class="card-subtitle mb-2 text-muted">Production report</h4>
-      <h4 class="card-subtitle mb-2 text-muted"><?= date("Y")?></h4> 
+      <br>
+        <br>
+
+
+       
+        <div class="monthContainer">
+          <form action="<?= base_url('index.php/annualController/readGoat'); ?>" method="post">
+          <div class="form-group">
+          <div class="select">  
+              <label for="sel1">Select year:</label>
+              
+              
+                
+              <select class="form-control"  name="year">
+                <option value="2019" selected >2019</option>
+                <option>2018 </option>
+                <option>2017 </option>
+                <option>2016 </option>
+                <option>2015 </option>
+                <option>2014 </option>
+                <option>2013 </option>
+              </select>
+          </div> 
+
+              <div class="select">
+              <label for="sel1">Select month:</label>
+              <select class="form-control" name="month">
+                <option value="January" selected  >January</option>
+                <option>January </option>
+                <option>February </option>
+                <option>March </option>
+                <option>April </option>
+                <option>May </option>
+                <option>June </option>
+                <option>July </option>
+                <option>August </option>
+                <option>September </option>
+                <option>October </option>
+                <option>November </option>
+                <option>December </option>
+              </select>
+ 
+      </div>
+            </div>
+        </div>
+
+        <input type="submit" value="Select" class="button">
+        </form>
+
         <div class="section mt-5">
         <?php
             $animals = 0 ;
