87b9c1a57d5b6fda01b580e9da961baac3f2a2cb
NipunaC95 <chandimal.nipuna.1995@gmail.com>
2020-10-01 19:37:57 +0530

    Update farmModel.php

--- application/models/farmModel.php
+++ application/models/farmModel.php
@@ -2,6 +2,7 @@
 
     class farmModel extends CI_Model{
         public function insert($data){
+			echo " Test";
             $this->load->helper('url');
             $this->db->insert('farms',$data);
             redirect("/main/enter");
